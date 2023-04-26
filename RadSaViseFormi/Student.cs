9fc8d531de4e7845ad6883e3036d1d9e8612b754
NinoMomic <58861649+NinoMomic@users.noreply.github.com>
2023-04-26 09:33:26 +0200

    Add files via upload

---
+++ RadSaViseFormi/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadSaViseFormi
+{
+    internal class Student
+    {
+        private string ime;
+        private string prezime;
+        private string brojIndeksa;
+        private DateTime datumRodjenja;
+        private string smjer;
+
+        private string Ime { get => ime; set => ime = value; }
+        private string BrojIndeksa { get => brojIndeksa; set => brojIndeksa = value;}
+        private DateTime DatumRodjenja { get => datumRodjenja; set => datumRodjenja = value; }
+        private string Prezime { get => prezime; set => prezime = value; }
+        private string Smjer { get => smjer; set => smjer = value; }
+
+        public override string ToString()
+        {
+            
+        }
+
+    }
+}
